5a4d3c9ede9d8ba8200cee40f580dc9f978484bf
Thanh Lam <dp1.nguyenthanhlam@gmail.com>
2020-03-19 15:14:36 +0700

    End Of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    [Authorize]
+    [Authorize] // Chỗ này chúng ta thiết lập Authorize (tức là cần có quyền hạn tương ứng mới được phép truy cập vào)
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -20,31 +20,27 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
-
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();  //Lấy tất cả row của entity Values từ DataContext
             return Ok(values);
         }
-
-        
-        [AllowAnonymous]
+        [AllowAnonymous]    // Chỗ này chúng ta thiết lập AllowAnonymous cho nên chúng ta có thể truy cập vào mà không cần có Quyền hạn tương ứng.
         [HttpGet("{id}")]
         public async Task<ActionResult> GetValue(int id)
         {
             var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);    //Lấy từ entity Values của DataContext row có Id = id nhập vào từ URL
             return Ok(value);
         }
-
         [HttpPost]
         public void Post([FromBody] string value)
         {}
-
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {}
-
         [HttpDelete("{id}")]
         public void Delete(int id)
         {}
